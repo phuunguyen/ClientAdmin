777641863fcb28c7f209383ac39403353fa62965
phuunguyen <phunguyen9129@gmail.com>
2019-11-08 08:20:18 +0700

    Update load tt giao hang

--- app/src/main/java/com/example/clientadmin/Fragment/Store/ThongTinGiaoHangFragment.java
+++ app/src/main/java/com/example/clientadmin/Fragment/Store/ThongTinGiaoHangFragment.java
@@ -10,7 +10,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,14 +37,13 @@ import java.util.ArrayList;
 public class ThongTinGiaoHangFragment extends Fragment {
     RecyclerView recyclerView;
     TrangThaiAdapter adapter;
-
+    SwipeRefreshLayout swipeRefreshLayout;
     ArrayList<Cart> data = new ArrayList<>();
 
     ImageButton btnXacNhanDH, btnGiaoHang, btnHoanThanh;
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference mData = database.getReference("Cart");
-
 
 
     View view;
@@ -60,12 +61,29 @@ public class ThongTinGiaoHangFragment extends Fragment {
         btnXacNhanDH = view.findViewById(R.id.cbXacNhanDH);
         btnHoanThanh = view.findViewById(R.id.cbHoanThanh);
         btnGiaoHang = view.findViewById(R.id.cbGiaoHang);
+        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_container);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         setEvent();
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override public void run() {
+                        data.clear();
+                        setEvent();
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                }, 2000);
+            }
+        });
+        swipeRefreshLayout.setColorScheme(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
     }
 
     private void setEvent() {
@@ -77,14 +95,14 @@ public class ThongTinGiaoHangFragment extends Fragment {
 
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                    if (id_Store.equals(dataSnapshot.child("id_store").getValue())) {
-                        Cart cart = new Cart();
-                        cart.setId_DonHang(dataSnapshot.child("id_donhang").getValue().toString());
-                        cart.setDate(dataSnapshot.child("ngaytao").getValue().toString());
+                if (id_Store.equals(dataSnapshot.child("id_store").getValue())) {
+                    Cart cart = new Cart();
+                    cart.setId_DonHang(dataSnapshot.child("id_donhang").getValue().toString());
+                    cart.setDate(dataSnapshot.child("ngaytao").getValue().toString());
 
-                        data.add(cart);
-                        adapter.notifyDataSetChanged();
-                    }
+                    data.add(cart);
+                    adapter.notifyDataSetChanged();
+                }
             }
 
             @Override
@@ -124,7 +142,6 @@ public class ThongTinGiaoHangFragment extends Fragment {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
     }
-
 
 
     @Override
